087bc144b87a9b830e734bf3ef34a4c36bfe02ea
Adam <leafkenangaclover@gmail.com>
2025-05-16 13:46:55 +0800

    Update 1.0

---
+++ app/Models/articles.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Dom\Comment;
+use Illuminate\Database\Eloquent\Model;
+
+class articles extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+}
